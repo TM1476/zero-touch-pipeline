813ef26afba056275c23fdf19b4462d20038ffcc
TradeXMatrix <tradexmatrxi@gmail.com>
2026-01-06 19:25:34 +0530

    Confirmed fix for prometheus metrics

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask 
+from prometheus_client import generate_latest, Counter 
+import os 
 app = Flask(__name__) 
+VIEW_COUNT = Counter('total_site_visits', 'Number of visits') 
 @app.route('/') 
-def hello(): return "Zero Touch Pipeline Active" 
-if __name__ == '__main__': app.run(host='0.0.0.0', port=5000) 
+def hello(): 
+    VIEW_COUNT.inc() 
+    return "Zero Touch Pipeline: Monitoring Active" 
+@app.route('/metrics') 
+def metrics(): 
+    return generate_latest() 
+if __name__ == '__main__': 
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port) 
